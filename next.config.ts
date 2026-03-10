46c6a4e7d985c6a66a384409c92b23d5e4c07209
virgcreat-ui <virgcreat@gmail.com>
2026-03-10 05:14:41 +0100

    Deploy static site

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/mission-control',
   async redirects() {
     return [
       {
